a65a2ab2ed0d30442f1fe2ce0642eb4703e549a0
BehiRm <behzad_rahimi@icloud.com>
2023-02-25 13:35:41 +0330

    learnclass

--- tut/main.py
+++ tut/main.py
@@ -1,9 +1,20 @@
 
 class Item:
-    pass
+    def __init__(self):
+        print("i am crated")
+    def calculate_totla_price(self, x, y):
+        return x * y
 
 
 item1 = Item()
 item1.name = "phone"
 item1.price = 100
 item1.quantity = 5
+
+
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 5
+
