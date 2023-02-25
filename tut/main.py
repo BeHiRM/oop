b47208a6838a7ff353b9e18b421164f0c592d19d
BehiRm <behzad_rahimi@icloud.com>
2023-02-25 13:49:02 +0330

    price metod

--- tut/main.py
+++ tut/main.py
@@ -13,5 +13,7 @@ item1 = Item("phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 5)
 
-print(item1.name, item1.price, item1.quantity, item1.calculate_totla_price)
-print(item2.name, item2.price, item2.quantity, item2.calculate_totla_price)
+print(item1.name, item1.price, item1.quantity,
+      item1.calculate_totla_price(item1.price, item1.quantity))
+print(item2.name, item2.price, item2.quantity,
+      item2.calculate_totla_price(item2.price, item2.quantity))
