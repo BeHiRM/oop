d0b34a0ff65f648c6a0566a94d4e011a607c9e59
BehiRm <behzad_rahimi@icloud.com>
2023-02-25 13:45:43 +0330

    update

--- tut/main.py
+++ tut/main.py
@@ -4,7 +4,7 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
-        
+
     def calculate_totla_price(self, x, y):
         return x * y
 
@@ -13,5 +13,5 @@ item1 = Item("phone", 100, 5)
 
 item2 = Item("Laptop", 1000, 5)
 
-print(item1.name ,item1.price, item1.quantity)
-print(item2.name ,item2.price, item2.quantity)
+print(item1.name, item1.price, item1.quantity, item1.calculate_totla_price)
+print(item2.name, item2.price, item2.quantity, item2.calculate_totla_price)
